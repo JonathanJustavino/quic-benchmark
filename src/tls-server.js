68413f617544eb4284f0e8de25827bb7e409515a
Jonathan L <jonathanl.@edr16592.dip.tu-dresden.de>
2020-11-04 17:23:36 +0100

    Add basic build setup

---
+++ src/tls-server.js
@@ -0,0 +1,14 @@
+import http from 'http';
+
+const HOST = "127.0.0.1";
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+})
+
+server.listen(PORT, HOST, () => {
+    console.log("Server running at http://${HOST}")
+})
