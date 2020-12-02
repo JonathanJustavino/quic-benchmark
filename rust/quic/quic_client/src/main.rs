c67b3a9baa53acfcd6665183a1bf297fdf239c94
Jonathan L <jonathanl.@Jonathans-MBP.fritz.box>
2020-12-02 19:55:42 +0100

    Add connect the client to the server socket & output for the error handling part

--- rust/quic/quic_client/src/main.rs
+++ rust/quic/quic_client/src/main.rs
@@ -1,18 +1,30 @@
+use std::net;
+
 fn main() -> std::io::Result<()> {
     println!("Hello, Client!");
-    let old_server_name = "quic-rust.org";
-    let server_name = "127.0.0.:7766";
+    let server_name = "quic-rust.org";
+    let _old_server_name = "127.0.0.:7766";
     let pem_cert = "../certs/cert.pem";
     let pem_key = "../certs/key.pem";
     let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION).unwrap();
     config.load_cert_chain_from_pem_file(pem_cert).unwrap();
     config.load_priv_key_from_pem_file(pem_key).unwrap();
     let socket = std::net::UdpSocket::bind("127.0.0.1:7755").unwrap();
+    let server_socket_address = net::SocketAddr::from(([127, 0, 0, 1], 7766));
+    let connection_result = socket.connect(server_socket_address);
+
+    match connection_result {
+        Ok(v) => {
+            println!("Successful connection: {:?}", v);
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+        }
+    }
+
     let scid = [0xba; 16];
     let mut conn = quiche::connect(Some(&server_name), &scid, &mut config).unwrap();
     let mut out = [0; 512];
-
-    // println!("{:?}", conn);
 
     loop {
         let write = match conn.send(&mut out) {
@@ -25,11 +37,13 @@ fn main() -> std::io::Result<()> {
     
             Err(e) => {
                 // An error occurred, handle it.
+                eprintln!("Error: {}", e);
                 break;
             },
         };
 
         let result = socket.send(&out[..write]);
+        println!("Writing");
         println!("{:?}", result);
     }
 
