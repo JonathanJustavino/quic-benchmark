0d4e41a234e0d6fba42e537e6a25a19f4a360313
nine <jeannineborn@aol.com>
2021-01-03 03:50:15 +0100

    refactor tcp-tls-client

--- nodejs-server-client/tcp-tls-client.js
+++ nodejs-server-client/tcp-tls-client.js
@@ -4,7 +4,7 @@ var tls = require('tls');
 var fs = require('fs');
 
 const PORT = 1337;
-const HOST = '192.168.52.36'
+const HOST = '192.168.52.36';
 
 // Pass the certs to the server and let it know to process even unauthorized certs.
 var options = {
@@ -13,26 +13,26 @@ var options = {
     rejectUnauthorized: false
 };
   
-var client = tls.connect(PORT, HOST, options, function() {
+var client_socket = tls.connect(PORT, HOST, options, function() {
     // Check if the authorization worked
-    if (client.authorized) {
+    if (client_socket.authorized) {
         console.log("Connection authorized by a Certificate Authority.");
     } else {
-        console.log("Connection not authorized: " + client.authorizationError)
+        console.log("Connection not authorized: " + client_socket.authorizationError)
     }
     
-    client.write("I am the client sending you a message..");
-    client.write("sending data..");
+    client_socket.write("I am the client sending you a message..");
+    client_socket.write("sending data..");
     
-    client.end();
+    client_socket.end();
 });
 
-client.on("data", function(data) {
+client_socket.on("data", function(data) {
     console.log('Received: %s [it is %d bytes long]',
         data.toString().replace(/(\n)/gm,""),
         data.length);
 });
 
-client.on('close', function() {
+client_socket.on('close', function() {
     console.log("Connection closed");
 });
