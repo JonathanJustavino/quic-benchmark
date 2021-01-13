23e982b01e067101a17489caef7216d2d27088a4
nine <jeannineborn@aol.com>
2021-01-13 17:26:31 +0100

    Refactor tcp code base and validate input parameter

--- tcp-tls-client.js
+++ tcp-tls-client.js
@@ -7,36 +7,6 @@ var net = require('net');
 const PORT = 1337;
 var HOST = "";
 
-validateIP();
-
-function validateIP() {
-    if (process.argv.length < 3) {
-        console.log("Too few arguments");
-        process.exit();
-    }
-
-    var local = process.argv[2];
-    var ip = process.argv[3];
-
-    if(local == "true") {
-        HOST = "localhost";
-        return;
-    }
-
-    if (local == "false") {
-        if (ip){
-            HOST = ip;
-        }
-        else {
-            HOST = '192.168.52.36';
-        }
-        return;
-    }
-    
-    console.log("Wrong commandline argument");
-    process.exit();
-}
-
 var options = {
     key: fs.readFileSync('certs/tcp-tls/private-key.pem'),
     cert: fs.readFileSync('certs/tcp-tls/public-cert.pem'),
@@ -49,46 +19,58 @@ var EventTimeStamps = {
     error: '',
 }
 
-var client_socket = tls.connect(PORT, HOST, options, function() {});
+function createClient(host, port, options) {
+    console.log(`HOST: ${HOST} PORT: ${PORT}`);
+    return tls.connect(PORT, HOST, options, function() {});
+}
 
-client_socket.on('ready', () => {
-    EventTimeStamps.ready = new Date();
-});
-
-client_socket.on('secureConnect', () => {
-    EventTimeStamps.writeToServer = new Date();
-    client_socket.write("I am the client sending you a message..");
-    client_socket.end();
-
-    const data = JSON.stringify(EventTimeStamps);
-    fs.writeFile('./tcp-benchmark-client.json', data, 'utf8', (err) => {
-        if (err) {
-            console.log(`Error writing file: ${err}`);
-        } else {
-            console.log(`File is written successfully!`);
-        }
+function registerEventHandlers(client_socket) {
+    client_socket.on('ready', () => {
+        EventTimeStamps.ready = new Date();
     });
-});
 
-client_socket.on('error', () => {
-    EventTimeStamps.error = new Date();
-    console.log("\nTLSSocket was destroyed with an error");
-});
+    client_socket.on('secureConnect', () => {
+        EventTimeStamps.writeToServer = new Date();
+        client_socket.write("I am the client sending you a message..");
+        client_socket.end();
+        
+        const data = JSON.stringify(EventTimeStamps);
+        fs.writeFile('./tcp-benchmark-client.json', data, 'utf8', (err) => {
+            if (err) {
+                console.log(`Error writing file: ${err}`);
+            } else {
+                console.log(`File is written successfully!`);
+            }
+        });
+    });
+
+    client_socket.on('error', () => {
+        EventTimeStamps.error = new Date();
+        console.log("\nTLSSocket was destroyed with an error");
+    });
+}
 
 function validateIP() {
-    if (process.argv.length < 3) {
+    if (process.argv.length < 2) {
         console.log("Too few arguments");
         process.exit();
     }
 
-    var local = process.argv[2];
+    var ip = process.argv[2];
+    var ipRegex = /\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+    var localIpRegex = /local/;
 
-    if(local == "true") {
+    if(localIpRegex.test(ip)) {
         HOST = "localhost";
         return;
     }
 
-    if (local == "false") {
+    if (ipRegex.test(ip)) {
+        HOST = ip;
+        return;
+    }
+
+    if (ip == null) {
         HOST = '192.168.52.36';
         return;
     }
@@ -96,3 +78,7 @@ function validateIP() {
     console.log("Wrong commandline argument");
     process.exit();
 }
+
+validateIP();
+var clientSocket = createClient(HOST, PORT, options);
+registerEventHandlers(clientSocket);
--- tcp-tls-server.js
+++ tcp-tls-server.js
@@ -4,24 +4,30 @@ const tls = require('tls');
 const fs = require('fs');
 const hexdump = require('hexdump-nodejs');
 
-validateIP();
 const PORT = 1337;
 var HOST = ""
 
 function validateIP() {
-    if (process.argv.length < 3) {
+    if (process.argv.length < 2) {
         console.log("Too few arguments");
         process.exit();
     }
 
-    var local = process.argv[2];
+    var ip = process.argv[2];
+    var ipRegex = /\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+    var localIpRegex = /local/;
 
-    if(local == "true") {
+    if(localIpRegex.test(ip)) {
         HOST = "localhost";
         return;
     }
 
-    if (local == "false") {
+    if (ipRegex.test(ip)) {
+        HOST = ip;
+        return;
+    }
+
+    if (ip == null) {
         HOST = '192.168.52.36';
         return;
     }
@@ -55,57 +61,66 @@ var measurements = {
     },
 }
 
-var server = tls.createServer(options, function(socket) {
-    socket.on('data', function(data) {
-        measurements.events.data = new Date();
-        console.log('Received: %s [it is %d bytes long]',
-        data.toString().replace(/(\n)/gm,""),
-        data.length);
+function createServer(host, port, options) {
+    console.log(`Server running at: ${host}:${port}`)
+    var server = tls.createServer(options, function(socket) {
+        socket.on('data', function(data) {
+            measurements.events.data = new Date();
+            console.log('Received: %s [it is %d bytes long]',
+            data.toString().replace(/(\n)/gm,""),
+            data.length);
+        });
+    
+        socket.on('end', () => {
+            measurements.events.streamEnd = new Date();
+        });
+        
+        socket.on('close', () => {
+            measurements.events.streamClose = new Date();
+            server.close();
+        });
     });
+    return server;
+}
 
-    socket.on('end', () => {
-        measurements.events.streamEnd = new Date();
+function registerEventHandler(server, keylogFlag) {
+    server.on('connection', () => {
+        measurements.hrTimeStamps.beforeHandshake = process.hrtime();
+    });
+    server.on('listening', function(error) { measurements.events.listening = new Date(); });
+    
+    server.on('newSession', (sessionID, sessionData, callback) => {
+        measurements.events.session = new Date();
     });
     
-    socket.on('close', () => {
-        measurements.events.streamClose = new Date();
-        server.close();
-    });
-});
-
-server.on('connection', () => {
-    measurements.hrTimeStamps.beforeHandshake = process.hrtime();
-});
-
-server.listen(PORT, HOST, function() {});
-
-server.on('listening', function(error) {
-    measurements.events.listening = new Date();
-});
-
-server.on('newSession', (sessionID, sessionData, callback) => {
-    measurements.events.session = new Date();
-});
-
-server.on('keylog', (line) => {
-    measurements.events.keylog = new Date();
-    console.log(hexdump(line));
-});
-
-server.on('secureConnection', (socket) => {
-    measurements.events.secure = new Date();
-    measurements.hrTimeStamps.afterHandshake = process.hrtime();
-    measurements.durations.handshakeDurationInNs = measurements.hrTimeStamps.afterHandshake[1] - measurements.hrTimeStamps.beforeHandshake[1];
-});
-
-server.on('close', () => {
-    measurements.events.socketClose = new Date();
-    const data = JSON.stringify(measurements);
-    fs.writeFile('./tcp-benchmark-server.json', data, 'utf8', (err) => {
-        if (err) {
-            console.log(`Error writing file: ${err}`);
-        } else {
-            console.log(`File is written successfully!`);
+    server.on('keylog', (line) => {
+        measurements.events.keylog = new Date();
+        if(keylogFlag) {
+            console.log(hexdump(line));
         }
     });
-});
+    
+    server.on('secureConnection', (socket) => {
+        measurements.events.secure = new Date();
+        measurements.hrTimeStamps.afterHandshake = process.hrtime();
+        measurements.durations.handshakeDurationInNs = measurements.hrTimeStamps.afterHandshake[1] - measurements.hrTimeStamps.beforeHandshake[1];
+    });
+    
+    server.on('close', () => {
+        measurements.events.socketClose = new Date();
+        const data = JSON.stringify(measurements);
+        fs.writeFile('./tcp-benchmark-server.json', data, 'utf8', (err) => {
+            if (err) {
+                console.log(`Error writing file: ${err}`);
+            } else {
+                console.log(`File is written successfully!`);
+            }
+        });
+    });
+}
+
+validateIP();
+var server = createServer(HOST, PORT, options);
+server.listen(PORT, HOST, function() {});
+var keylogFlag = false;
+registerEventHandler(server, keylogFlag);
