2241365e38cfe8f9b02c14b27b3479a41cbaa20f
nine <jeannineborn@aol.com>
2021-01-16 20:12:45 +0100

    Refactor quic code base and validate input parameter

--- quic-client.js
+++ quic-client.js
@@ -5,6 +5,7 @@ const hexdump = require('hexdump-nodejs');
 const key  = fs.readFileSync('certs/quic/server.key');
 const cert = fs.readFileSync('certs/quic/server.crt');
 const ca   = fs.readFileSync('certs/quic/server.csr');
+var HOST;
 
 var EventTimeStamps = {
   ready: '',
@@ -12,43 +13,87 @@ var EventTimeStamps = {
   error: '',
 }
 
-const client_socket = createQuicSocket({
-  client: {
-    key,
-    cert,
-    ca,
-    requestCert: true,
-    alpn: 'hello'
+function validateIP() {
+  if (process.argv.length < 2) {
+      console.log("Too few arguments");
+      process.exit();
   }
-});
 
-const client_session = client_socket.connect({
-  address: '192.168.52.38',
-  port: 1234,
-});
+  var ip = process.argv[2];
+  var ipRegex = /\d{2,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+  var localIpRegex = /local/;
 
-client_socket.on('ready', () => {
-  EventTimeStamps.ready = new Date();
-});
+  if(localIpRegex.test(ip)) {
+      HOST = "localhost";
+      return;
+  }
 
-client_session.on('secure', () => {
-  const stream = await client_session.openStream();
+  if (ipRegex.test(ip)) {
+      HOST = ip;
+      return;
+  }
 
-  EventTimeStamps.writeToServer = new Date();
-  stream.write("I am the client sending you a message..");
-  stream.end();
-  client_socket.close();
-  const data = JSON.stringify(EventTimeStamps);
-  fs.writeFile('./quic-benchmark-client.json', data, 'utf8', (err) => {
-    if (err) {
-      console.log(`Error writing file: ${err}`);
-    } else {
-      console.log(`File is written successfully!`);
+  if (ip == null) {
+      HOST = '192.168.52.36';
+      return;
+  }
+  
+  console.log("Wrong commandline argument");
+  process.exit();
+}
+
+function createClient() {
+  return createQuicSocket({
+    client: {
+      key,
+      cert,
+      ca,
+      requestCert: true,
+      alpn: 'hello'
     }
   });
-});
+}
 
-client_socket.on('error', () => {
-  EventTimeStamps.error = new Date();
-  console.log("\nQuicSocket was destroyed with an error");
-});
+function createClientSession(client_socket, host) {
+  console.log(host);
+  return client_socket.connect({
+    address: host,
+    port: 1234,
+  });
+}
+
+function registerSocketEventHandler(client_socket) {
+  client_socket.on('ready', () => {
+    EventTimeStamps.ready = new Date();
+  });
+
+  client_socket.on('error', () => {
+    EventTimeStamps.error = new Date();
+    console.log("\nQuicSocket was destroyed with an error");
+  });
+}
+
+function registerSessionEventHandler(client_socket, client_session){
+  client_session.on('secure', async () => {
+    const stream = await client_session.openStream();
+  
+    EventTimeStamps.writeToServer = new Date();
+    stream.write("I am the client sending you a message..");
+    stream.end();
+    client_socket.close();
+    const data = JSON.stringify(EventTimeStamps);
+    fs.writeFile('./quic-benchmark-client.json', data, 'utf8', (err) => {
+      if (err) {
+        console.log(`Error writing file: ${err}`);
+      } else {
+        console.log(`File is written successfully!`);
+      }
+    });
+  });
+}
+
+validateIP();
+var client = createClient();
+registerSocketEventHandler(client);
+var client_session = createClientSession(client, HOST);
+registerSessionEventHandler(client, client_session);
--- quic-server.js
+++ quic-server.js
@@ -24,65 +24,72 @@ var measurements = {
   }
 }
 
-// Create the QUIC UDP IPv4 socket bound to local IP port 1234
-const server_socket = createQuicSocket({ endpoint: { port } });
+function createServer(port) {
+  // Create the QUIC UDP IPv4 socket bound to local IP port 1234
+  console.log(port);
+  return createQuicSocket({ endpoint: { port } });
+}
 
+function registerEventHandler(server_socket) {
+  server_socket.on('listening', () => {
+    measurements.events.listening = new Date();
+  });
+      
+  server_socket.on('session', (session) => {
+    measurements.events.session = new Date();
+    
+    session.on('keylog', (line) => {
+      measurements.events.keylog = new Date();
+      console.log(hexdump(line));
+    });
+  
+    session.on('secure', () => {
+      measurements.events.secure = new Date();
+    });
+  
+    session.on('stream', (stream) => {
+      stream.on('data', function(data) {
+        measurements.events.data = new Date();
+        console.log('Received: %s [it is %d bytes long]',
+        data.toString().replace(/(\n)/gm,""),
+        data.length);
+      });
+      
+      stream.on('end', () => {
+        measurements.events.streamEnd = new Date();
+      });
+      
+      stream.on('close', () => {
+        measurements.events.streamClose = new Date();
+        measurements.durations.handshakeDurationInNs = session.handshakeDuration.toString();
+      });
+    });
+    
+    session.on('close', () => {
+      console.log("number of bytes received for the socket: " + server_socket.bytesReceived);
+      console.log("number of bytes sent from the socket: " + server_socket.bytesSent);
+      console.log("number of packets received for the socket: " + server_socket.packetsReceived);
+      console.log("number of packets sent from the socket: " + server_socket.packetsSent);
+      server_socket.close();
+    });
+  });
+  
+  server_socket.on('close', () => {
+    measurements.events.socketClose = new Date();
+    const data = JSON.stringify(measurements);
+    fs.writeFile('./quic-benchmark-server.json', data, 'utf8', (err) => {
+      if (err) {
+        console.log(`Error writing file: ${err}`);
+      } else {
+        console.log(`File is written successfully!`);
+      }
+    });
+  });
+}
+
+var server = createServer(port);
 // Tell the socket to operate as a server_socket using the given
 // key and certificate to secure new connections, using
 // the fictional 'hello' application protocol.
-server_socket.listen({ key, cert, alpn: 'hello' });
-
-server_socket.on('listening', () => {
-  measurements.events.listening = new Date();
-});
-    
-server_socket.on('session', (session) => {
-  measurements.events.session = new Date();
-  
-  session.on('keylog', (line) => {
-    measurements.events.keylog = new Date();
-    console.log(hexdump(line));
-  });
-
-  session.on('secure', () => {
-    measurements.events.secure = new Date();
-  });
-
-  session.on('stream', (stream) => {
-    stream.on('data', function(data) {
-      measurements.events.data = new Date();
-      console.log('Received: %s [it is %d bytes long]',
-      data.toString().replace(/(\n)/gm,""),
-      data.length);
-    });
-    
-    stream.on('end', () => {
-      measurements.events.streamEnd = new Date();
-    });
-    
-    stream.on('close', () => {
-      measurements.events.streamClose = new Date();
-      measurements.durations.handshakeDurationInNs = session.handshakeDuration.toString();
-    });
-  });
-  
-  session.on('close', () => {
-    console.log("number of bytes received for the socket: " + server_socket.bytesReceived);
-    console.log("number of bytes sent from the socket: " + server_socket.bytesSent);
-    console.log("number of packets received for the socket: " + server_socket.packetsReceived);
-    console.log("number of packets sent from the socket: " + server_socket.packetsSent);
-    server_socket.close();
-  });
-});
-
-server_socket.on('close', () => {
-  measurements.events.socketClose = new Date();
-  const data = JSON.stringify(measurements);
-  fs.writeFile('./quic-benchmark-server.json', data, 'utf8', (err) => {
-    if (err) {
-      console.log(`Error writing file: ${err}`);
-    } else {
-      console.log(`File is written successfully!`);
-    }
-  });
-});
+server.listen({ key, cert, alpn: 'hello' });
+registerEventHandler(server);
