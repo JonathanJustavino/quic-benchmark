77a2e9b1a02e62db9d7049b7999c7b637876224f
nine <jeannineborn@aol.com>
2021-01-19 18:43:53 +0100

    Write pyshark output to file

--- traffic-analysis/pshark.py
+++ traffic-analysis/pshark.py
@@ -4,11 +4,13 @@ import pyshark
 interface = 'wlp61s0'
 #filter = 'tcp port 1337'
 #filter = 'udp port 1234'
-
+# bpf_filter=filter
+port_type = "tcp"
 
 def capture_packets(interface, filter):
+    capture_file = f"{port_type}.cap"
     print("capturing packets..")
-    capture = pyshark.LiveCapture(interface=interface, display_filter=filter)
+    capture = pyshark.LiveCapture(interface=interface, output_file=capture_file)
     capture.sniff(timeout=20)
     print(capture)
 
