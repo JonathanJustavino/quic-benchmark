9dff2a1f8b5ada5b3ae64322563cc3c341983f6d
Amy <amelie.wagner@mailbox.tu-dresden.de>
2021-01-20 13:42:14 +0100

    updated file for parsing content of .cap file. I also added my Intellij settings LOCALLY, I hope the gitignore works and they don't get pushed as well.

---
+++ traffic-analysis/parse_capfile.py
@@ -0,0 +1,45 @@
+import pyshark
+
+# packet.layer.field_names: alle möglichen Attribute einer Layer zB destination adresse usw.
+
+if __name__ == '__main__':
+    cap = pyshark.FileCapture('/home/amelie/Uni/RNP_Komplexpraktikum/quic-benchmark/traffic-analysis/tcp.cap')
+
+    # pyshark.packet.packet.Packet
+    packet = cap[5]
+
+    # pyshark.packet.packet.Packet -> dict,
+    # weil im "pyshark.packet.packet.Packet" nicht ganz klar ist,
+    # wie die keys zu den values heißen
+    vars_packet = vars(packet)
+
+    # alle keys in packet-dict
+    print(vars_packet.keys())
+
+    # alle Attribute in pyshark.packet.packet.Packet
+    print("Attribute in pyshark.packet.packet.Packet:", packet.__dict__)
+
+    # Gesamte Paketlänge, die gecaptured wurde
+    capt_pack_lenght = vars_packet['captured_length']
+    print('Total captured packet length: ', capt_pack_lenght)
+
+    # ----  Get different layers: ------
+
+    # Ethernet layer -> kp wie das Attribut im "pyshark.packet.packet.Packet" heißt,
+    # deshalb hole ich die aus dem dict
+    layer_ethernet = vars_packet['layers'][0]
+
+    # IP Layer -> steht in Doku dass man sie mit 'IP' über "pyshark.packet.packet.Packet" erreicht
+    layer_ip = packet['IP']
+
+    # UDP Layer
+    layer_udp = vars_packet['layers'][2]
+
+    # PAYLOAD / Data
+    payload = vars_packet['layers'][3]
+    print('payload länge: ', payload.len)
+
+    # ---- overhead berechnen ----
+    # Overhead = (gesamtpaket länge) - (payload länge)
+    overhead = int(capt_pack_lenght) - int(payload.len)
+    print("overhead: ", overhead)
