6690eff349d659b2f1edd21386e75a13a11524a6
nine <jeannineborn@aol.com>
2021-01-20 12:24:25 +0100

    Set correct interface and filter for wireshark

--- traffic-analysis/pshark.py
+++ traffic-analysis/pshark.py
@@ -1,18 +1,40 @@
 import pyshark
 
 
-interface = 'wlp61s0'
-#filter = 'tcp port 1337'
-#filter = 'udp port 1234'
-# bpf_filter=filter
+interface = ''
+interface_lo = 'lo'
+interface_docker = 'br-bbd147c17183'
+filter = ''
+filter_tcp = 'tcp port 1337'
+filter_udp = 'udp port 1234'
+
 port_type = "tcp"
 
-def capture_packets(interface, filter):
+temp_flag = False
+tcp_filter = True
+
+if temp_flag:
+    interface = interface_docker
+else: 
+    interface = interface_lo
+
+if tcp_filter:
+    filter = filter_tcp
+else:
+    filter = filter_udp
+
+
+def capture_packets(interface, filter=""):
     capture_file = f"{port_type}.cap"
     print("capturing packets..")
-    capture = pyshark.LiveCapture(interface=interface, output_file=capture_file)
-    capture.sniff(timeout=20)
-    print(capture)
+    capture = pyshark.LiveCapture(interface=interface, bpf_filter=filter, output_file=capture_file)
+    #capture = pyshark.LiveCapture(interface=interface, output_file=capture_file)
+    #capture.sniff(timeout=20)
+    #print(capture)
+
+    for packet in capture.sniff_continuously(packet_count=9):
+        print(f"p: {packet.highest_layer}")
+
 
 
 def gather_info():
