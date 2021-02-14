5dd87fe40a6cbe2a0ae4e9f7fefd6c0ee1005eaa
nine <jeannineborn@aol.com>
2021-02-14 22:11:53 +0100

    Change parser to set ip dynamic not constant

--- parser.py
+++ parser.py
@@ -7,6 +7,5 @@ dockerParser.add_argument('-t', '--tcp', action='store_true', help='Use tcp-tls 
 dockerParser.add_argument('-q', '--quic', action='store_true', help='Use quic socket')
 dockerParser.add_argument('-s', '--server', action='store_true', help='Run server')
 dockerParser.add_argument('-c', '--client', action='store_true', help='Run client')
-dockerParser.add_argument('-ip', '--ipaddress', action='store_const', 
-                    const="", default="", 
+dockerParser.add_argument('-ip', '--ipaddress', action='store', default="", 
                     help='Define a remote ip to the machine where server is running')
