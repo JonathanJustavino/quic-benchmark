a8c8d5ed35470c62681250d9e325bcb1f1b00c55
nine <jeannineborn@aol.com>
2021-02-14 21:52:58 +0100

    Pick benchmark based on parser

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import asyncio
 import subprocess
-from benchmarks import local_benchmark, quic_benchmark, tcp_benchmark
+from benchmarks import local_benchmark, remote_benchmark, quic_benchmark, tcp_benchmark
+from parser import dockerParser
 
 
 log_helper = "-----------------------------------------"
@@ -14,13 +15,30 @@ async def compose_down():
     subprocess.run("docker-compose down", shell=True)
 
 
+def run_benchmark():
+    arguments = dockerParser.parse_args()
+    print(arguments)
+    if arguments.quic:
+        benchmark = quic_benchmark
+    elif arguments.tcp:
+        benchmark = tcp_benchmark
+
+    if not arguments.ipaddress and not arguments.server:
+        local_benchmark(*benchmark)
+    else:
+        if arguments.server:
+            remote_benchmark(benchmark[0] , server=True)
+        elif arguments.client:
+            remote_benchmark(benchmark[0], arguments.ipaddress, client=True)
+    
+
 async def main():
     print("Docker Compose Up")
     await compose_up()
     print("Container Setup Finished")
     print(log_helper)
     print("Container Output:")
-    local_benchmark(*tcp_benchmark)
+    run_benchmark()
     print(log_helper)
     print("Docker Compose Down")
     await compose_down()
