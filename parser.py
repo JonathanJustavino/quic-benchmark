5a298c34eadd125fbbd9f24af0dfb7a373a9cad3
Jonathan L <jonathanl.@Daedalus.fritz.box>
2021-02-16 18:42:52 +0100

    Add parser for ping

--- parser.py
+++ parser.py
@@ -9,3 +9,10 @@ dockerParser.add_argument('-s', '--server', action='store_true', help='Run serve
 dockerParser.add_argument('-c', '--client', action='store_true', help='Run client')
 dockerParser.add_argument('-ip', '--ipaddress', action='store', default="", 
                     help='Define a remote ip to the machine where server is running')
+
+pingParser = argparse.ArgumentParser(description='Run ping with or without network usage check')
+dockerParser.add_argument('-c', '--check', action='store_true', help='Run ping and check if the \
+    network usage is below threshold')
+dockerParser.add_argument('-p', '--ping', action='store_true', help='Run ping and document output')
+dockerParser.add_argument('-t', '--threshold', action='store', default="1ms", 
+                    help='Define a threshold for network  usage')
