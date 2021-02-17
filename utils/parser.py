9b348c6698483ebc0ef56d871cab026cf6386f85
nine <jeannineborn@aol.com>
2021-02-18 00:31:17 +0100

    Add traffic control functionality

--- utils/parser.py
+++ utils/parser.py
@@ -7,9 +7,14 @@ dockerParser.add_argument('-t', '--tcp', action='store_true', help='Use tcp-tls 
 dockerParser.add_argument('-q', '--quic', action='store_true', help='Use quic socket')
 dockerParser.add_argument('-s', '--server', action='store_true', help='Run server')
 dockerParser.add_argument('-c', '--client', action='store_true', help='Run client')
-dockerParser.add_argument('--clean', action='store_true', help='Remove all measurement files')
 dockerParser.add_argument('-ip', '--ipaddress', action='store', default="", 
                     help='Define a remote ip to the machine where server is running')
+dockerParser.add_argument('-d', '--delay', action='store', default=0, 
+                    help='Simulate network delay in ms')
+dockerParser.add_argument('--threshold', action='store', default=1, 
+                    help='Define a threshold for network  usage')
+dockerParser.add_argument('--clean', action='store_true', help='Remove all measurement files')
+
 
 pingParser = argparse.ArgumentParser(description='Run ping with or without network usage check')
 pingParser.add_argument('-c', '--check', action='store_true', help='Run ping and check if the \
