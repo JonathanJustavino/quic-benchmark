d4044b93a87611ab795b7b6b4dd783078deb13e2
Jonathan L <s3190208@mail.zih.tu-dresden.de>
2021-03-10 16:32:10 +0100

    Rework dataloader to support individual files

--- visualize_events/dataloader.py
+++ visualize_events/dataloader.py
@@ -1,4 +1,3 @@
-# from ..utils.measurements import get_folder_path
 import os
 import json
 from dateutil import parser, relativedelta
@@ -12,23 +11,22 @@ def load_folders(socket_type, path):
     if osx_file in folders:
         folders.remove(osx_file)
 
-    add_path = lambda f: f"{path}{f}"
-    folders = list(map(add_path, folders))
+    folders.sort()
     return folders
 
 
-def read_results(folders, socket_type, results):
+def read_results(path, folders, socket_type, results):
     server = f"{socket_type}-benchmark-server.json"
     client = f"{socket_type}-benchmark-client.json"
     for folder in folders:
-        client_path = f"{folder}/{client}"
-        server_path = f"{folder}/{server}"
-        accumulate_client_data(client_path, results)
-        accumulate_server_data(server_path, results)
+        client_path = f"{path}/{socket_type}/remote/{folder}/{client}"
+        server_path = f"{path}/{socket_type}/remote/{folder}/{server}"
+        accumulate_client_data(client_path, results, folder)
+        accumulate_server_data(server_path, results, folder)
         accumulate_ready_to_session(client_path, server_path, results)
 
 
-def accumulate_data(data, results, side='server'):
+def accumulate_data(data, results, folder, side='server'):
     for key_1, value_1 in data:
         for key_2, value_2 in data:
             if key_1 is key_2 or not value_1 or not value_2:
@@ -37,24 +35,23 @@ def accumulate_data(data, results, side='server'):
             date_2 = parser.parse(value_2)
             if date_1 < date_2:
                 new_key = f"{key_1}-to-{key_2}"
-                if new_key in results[side]:
-                    results[side][f"{key_1}-to-{key_2}"] += relativedelta.relativedelta(date_2, date_1).microseconds
-                    continue
-                results[side][f"{key_1}-to-{key_2}"] = relativedelta.relativedelta(date_2, date_1).microseconds
+                if new_key not in results[side]:
+                    results[side][f"{key_1}-to-{key_2}"] = {folder: 0}
+                results[side][f"{key_1}-to-{key_2}"][f"{folder}"] = relativedelta.relativedelta(date_2, date_1).microseconds
     
 
-def accumulate_client_data(file, results):
+def accumulate_client_data(file, results, folder):
     with open(file, 'r') as f:
         data = json.load(f).items()
-        accumulate_data(data, results, side='client')
+        accumulate_data(data, results, folder, side='client')
 
 
-def accumulate_server_data(file, results):
+def accumulate_server_data(file, results, folder):
     duration = 'handshakeDuration'
     with open(file, 'r') as f:
         data = json.load(f)
         events = data["events"].items()
-        accumulate_data(events, results)
+        accumulate_data(events, results, folder)
         if duration not in results:
             results[duration] = float(data["durations"]["handshakeDurationInNs"]) / 1000
             return
@@ -76,9 +73,11 @@ def accumulate_ready_to_session(client_path, server_path, results):
 
 def average_results(results, count):
     for key, _ in results['server'].items():
-        results['server'][key] /= count
+        values = results['server'][key].values()
+        results['server'][key]['avg'] = sum(values) / len(values)
     for key, _ in results['client'].items():
-        results['client'][key] /= count
+        values = results['client'][key].values()
+        results['client'][key]['avg'] = sum(values) / len(values)
     results['handshakeDuration'] /= count
     results['ready-to-session'] /= count
 
@@ -87,6 +86,6 @@ def load_results(socket_type, path):
     results = {'ping': {}, 'server': {}, 'client': {}}
     folders = load_folders(socket_type, path)
     folder_count = len(folders)
-    read_results(folders, socket_type, results)
+    read_results(path, folders, socket_type, results)
     average_results(results, folder_count)
     return results
